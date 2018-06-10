f50fd45944cb17d145ec80b5323c81e3933662d5
Khurram Shahzad <macpro@macpros-MBP-2.home>
2018-06-10 13:24:28 +0100

    console app

---
+++ f#-console/ConsoleApplication1/CSVService.fs
@@ -0,0 +1,19 @@
+﻿module CSVService
+
+open FSharp.Data
+
+type sample = CsvProvider<"sample.csv">
+
+type Result = Success | Fail
+
+type SampleService() =
+ member this.GetOnly100Rows = 
+    let data = new sample() 
+    query {
+            for row in data.Rows do
+            take 100
+        }
+
+  member this.Search search = 
+    use data = new sample()
+    if data.Rows |> Seq.exists(fun i -> i.PolicyID = search) then Success else Fail
---
+++ f#-console/ConsoleApplication1/Program.fs
@@ -0,0 +1,12 @@
+﻿module Program
+open System
+open CSVService
+let service = new SampleService()
+
+[<EntryPoint>]
+let main argv =
+    service.GetOnly100Rows 
+    |> Seq.iter(fun p-> printfn "%A" p) 
+    |> Console.Read 
+    |> ignore
+    0
